52312cbbff6bc3dcc4474c290d52119ea9d8aa4e
SheylaAlv <35314184+SheylaAlv@users.noreply.github.com>
2019-11-12 15:48:23 -0800

    Fized size of vars

--- mux8by1.v
+++ mux8by1.v
@@ -1,8 +1,8 @@
-module mux8by1(sourceReg, inputVal, outputVal);
-input[15:0] sourceReg;
-input[7:0] inputVal;
+module mux8by1(select, inputVal, outputVal);
+input[2:0] select;
+input[15:0] inputVal;
 output outputVal;
 
-assign outputVal = inputVal[sourceReg];
+assign outputVal = inputVal[select];
 
 endmodule
