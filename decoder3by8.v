8da60faaf1cae62e2bc2edd53ad5312ad929858f
SheylaAlv <35314184+SheylaAlv@users.noreply.github.com>
2019-11-16 15:17:55 -0800

    Added mux code and test bench

--- decoder3by8.v
+++
@@ -1,7 +0,0 @@
-module decoder3by8(destReg, outReg);
-input[2:0] destReg;
-output [7:0] outReg;
-
-assign outReg = 8'b00000001 << (destReg);
-
-endmodule
--- mux8by1.v
+++
@@ -1,8 +0,0 @@
-module mux8by1(select, inputVal, outputVal);
-input[2:0] select;
-input[15:0] inputVal;
-output outputVal;
-
-assign outputVal = inputVal[select];
-
-endmodule
